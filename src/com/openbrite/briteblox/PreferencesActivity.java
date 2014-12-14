b41970a86539ee6e901356baad47666b052b604d
Stacy Wylie <childofthehorn@gmail.com>
2014-12-13 18:29:05 -0600

    Small fixes, use in horizontal
    Signed-off-by: Stacy Wylie <childofthehorn@gmail.com>

--- src/com/openbrite/briteblox/PreferencesActivity.java
+++ src/com/openbrite/briteblox/PreferencesActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.widget.Toast;
  
 public class PreferencesActivity extends PreferenceActivity {
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
